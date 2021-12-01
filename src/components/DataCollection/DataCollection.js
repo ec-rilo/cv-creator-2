ce8028db0a1c30018d2a28d7aa6d09a4e03d19eb
Edgar <ecarrillo046@gmail.com>
2021-12-01 12:52:34 -0800

    feat: add header

---
+++ src/components/DataCollection/DataCollection.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../../styles/DataCollection.css';
+
+class DataCollection extends Component {
+  render() {
+    return (
+      <div className="data-collection-container">
+        <header>
+          <h1>CV Creator</h1>
+          <p>Thank you for visiting! Please fill out the information below</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default DataCollection;

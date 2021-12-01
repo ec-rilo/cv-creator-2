365a747be7b287116a59e5342e989c31fa18f842
Edgar <ecarrillo046@gmail.com>
2021-12-01 13:49:32 -0800

    feat: add info-container to Personal Information

--- src/components/DataCollection/DataCollection.js
+++ src/components/DataCollection/DataCollection.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../../styles/DataCollection.css';
+import InfoContainer from './InfoContainer';
 
 class DataCollection extends Component {
   render() {
@@ -9,6 +10,16 @@ class DataCollection extends Component {
           <h1>CV Creator</h1>
           <p>Thank you for visiting! Please fill out the information below</p>
         </header>
+        <form action="#" method="#">
+          <InfoContainer
+            categoryName="Personal Information"
+            inputArr={[
+              { text: 'First Name', forProp: 'first-name' },
+              { text: 'Last Name', forProp: 'last-name' },
+              { text: 'Title', forProp: 'title' },
+            ]}
+          />
+        </form>
       </div>
     );
   }

1c95cf4c95ecd40eb40a04ec71a7144ad1899530
Edgar <ecarrillo046@gmail.com>
2021-12-01 14:43:13 -0800

    feat: add Category for personal info

--- src/components/DataCollection/DataCollection.js
+++ src/components/DataCollection/DataCollection.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../../styles/DataCollection.css';
 import InfoContainer from './InfoContainer';
+import Category from './Category';
 import uniqid from 'uniqid';
 
 class DataCollection extends Component {
@@ -12,14 +13,17 @@ class DataCollection extends Component {
           <p>Thank you for visiting! Please fill out the information below</p>
         </header>
         <form action="#" method="#">
-          <InfoContainer
-            categoryName="Personal Information"
-            inputArr={[
-              { text: 'First Name', forProp: 'first-name', key: uniqid() },
-              { text: 'Last Name', forProp: 'last-name', key: uniqid() },
-              { text: 'Title', forProp: 'title', key: uniqid() },
-            ]}
-          />
+          <div className="info-section">
+            <Category name="Personal Information" />
+            <InfoContainer
+              categoryName="Personal Information"
+              inputArr={[
+                { text: 'First Name', forProp: 'first-name', key: uniqid() },
+                { text: 'Last Name', forProp: 'last-name', key: uniqid() },
+                { text: 'Title', forProp: 'title', key: uniqid() },
+              ]}
+            />
+          </div>
         </form>
       </div>
     );

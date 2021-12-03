03cb4d778941cfcf18eec64068083163d2ed791c
Edgar <ecarrillo046@gmail.com>
2021-12-03 11:47:33 -0800

    feat: imported Header component and removed default header

--- src/components/DataCollection/DataCollection.js
+++ src/components/DataCollection/DataCollection.js
@@ -9,15 +9,13 @@ import DescriptionInput from './DescriptionInput';
 import uniqid from 'uniqid';
 import AddRemoveBtn from './AddRemoveBtn';
 import ResetBtn from './ResetBtn';
+import Header from './Header/Header';
 
 class DataCollection extends Component {
   render() {
     return (
       <div className="data-collection-container">
-        <header>
-          <h1>CV Creator</h1>
-          <p>Thank you for visiting! Please fill out the information below</p>
-        </header>
+        <Header />
         <form action="#" method="#">
           <div className="info-section">
             <Category name="Personal Information" />

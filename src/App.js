add326f0007f5a9fe2878566b4f40c7b2cc4d6a3
Edgar <ecarrillo046@gmail.com>
2021-12-02 17:57:33 -0800

    feat: add DataCollection component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-import './App.css';
+import './styles/reset.css';
+import './styles/App.css';
+import React, { Component } from 'react';
+import DataCollection from './components/DataCollection/DataCollection';
 
-function App() {}
+class App extends Component {
+  render() {
+    return (
+      <div className="main-container">
+        <DataCollection />
+        <div className="resume-container"></div>
+      </div>
+    );
+  }
+}
 
 export default App;

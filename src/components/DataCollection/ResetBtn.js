1667d9ec37591c440864c244a83b2bee0231852e
Edgar <ecarrillo046@gmail.com>
2021-12-02 19:19:18 -0800

    feat: add Reset Button component

---
+++ src/components/DataCollection/ResetBtn.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ResetBtn extends Component {
+  render() {
+    return (
+      <div className="reset-btn-container">
+        <button className="reset-btn" type="reset">
+          Reset
+        </button>
+      </div>
+    );
+  }
+}
+
+export default ResetBtn;

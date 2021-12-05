d9c3fa0e636251e3cd9aaef4d3900641d9f4a3e8
Edgar <ecarrillo046@gmail.com>
2021-12-04 21:51:49 -0800

    feat(ExperienceSection): test for rendering multiple ExperienceSections

--- src/components/DataCollection/__tests__/DataCollection.test.js
+++ src/components/DataCollection/__tests__/DataCollection.test.js
@@ -3,6 +3,14 @@ import '@testing-library/jest-dom';
 import DataCollection from '../DataCollection';
 
 describe('Experience Section', () => {
+  const addSections = (numOfSections) => {
+    for (let i = 0; i < numOfSections; ++i) {
+      const btns = screen.getAllByRole('button');
+      const addBtn = btns.find((btn) => btn.innerHTML === 'Add');
+      fireEvent.click(addBtn);
+    }
+  };
+
   test('can render 1 new section when "add" button is clicked', () => {
     render(<DataCollection />);
     const btns = screen.getAllByRole('button');
@@ -10,5 +18,12 @@ describe('Experience Section', () => {
     fireEvent.click(addBtn);
     const sections = screen.getAllByTitle('This is an Experience Section');
     expect(sections.length).toBe(2);
+  });
+
+  test('renders multiple(3) new sections when "add" button is clicked', () => {
+    render(<DataCollection />);
+    addSections(3);
+    const sections = screen.getAllByTitle('This is an Experience Section');
+    expect(sections.length).toBe(4);
   });
 });

2d8495652d4f2e43d2bec7ffcad7c6ae1bcbb436
Edgar <ecarrillo046@gmail.com>
2021-12-06 16:55:35 -0800

    feat: add about section

--- src/components/Resume/Sections/UpperSection/UpperSection.js
+++ src/components/Resume/Sections/UpperSection/UpperSection.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import UserImg from './UserImg';
+import About from './About';
 
 class UpperSection extends Component {
   render() {
     return (
       <div className="upper-section-container">
         <UserImg />
-        <div className="about-container"></div>
+        <About />
       </div>
     );
   }

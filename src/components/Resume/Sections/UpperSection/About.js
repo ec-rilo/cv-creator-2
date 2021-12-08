638475a90eaf53bb85fb7c12802bbde959e919a5
Edgar <ecarrillo046@gmail.com>
2021-12-08 08:56:05 -0800

    feat: add data prop

--- src/components/Resume/Sections/UpperSection/About.js
+++ src/components/Resume/Sections/UpperSection/About.js
@@ -3,10 +3,15 @@ import CategoryTitle from '../CategoryTitle';
 
 class About extends Component {
   render() {
+    const { data } = this.props;
     return (
       <div className="resume-section-container">
         <CategoryTitle text="About Me" />
-        <textarea className="about-text-container">{}</textarea>
+        <textarea
+          className="about-text-container"
+          value={data[0] === '' ? '' : data[0][0].description}
+          readOnly
+        ></textarea>
       </div>
     );
   }

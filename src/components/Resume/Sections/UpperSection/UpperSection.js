1b2860291d8baea0dbdae6dc033fab17f547f6cd
Edgar <ecarrillo046@gmail.com>
2021-12-08 08:56:34 -0800

    feat: add About component and data prop

--- src/components/Resume/Sections/UpperSection/UpperSection.js
+++ src/components/Resume/Sections/UpperSection/UpperSection.js
@@ -4,10 +4,11 @@ import About from './About';
 
 class UpperSection extends Component {
   render() {
+    const { data } = this.props;
     return (
       <div className="upper-section-container">
         <UserImg />
-        <About />
+        <About data={data} />
       </div>
     );
   }

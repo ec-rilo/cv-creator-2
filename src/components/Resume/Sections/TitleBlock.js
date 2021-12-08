8b8c2107ffe081643d142a06e4056fb4b10e4e84
Edgar <ecarrillo046@gmail.com>
2021-12-08 10:06:20 -0800

    feat: create TitleBlock

---
+++ src/components/Resume/Sections/TitleBlock.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class TitleBlock extends Component {
+  render() {
+    return (
+      <div className="title-block">
+        <div>
+          <p></p>
+        </div>
+        <div>
+          <p></p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TitleBlock;

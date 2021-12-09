8a8bbf0b0cfdc847c4567d7e42edd2c57eb9bf31
Edgar <ecarrillo046@gmail.com>
2021-12-09 13:14:32 -0800

    feat: add CategorySection components for each item in Contact

--- src/components/Resume/Sections/LowerSection/InfoBlock/Sections/Contact.js
+++ src/components/Resume/Sections/LowerSection/InfoBlock/Sections/Contact.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 import CategoryTitleRight from './CategoryTitleRight';
+import phoneIcon from '../../../../../../images/resume/contact-icons/phone-icon.png';
+import emailIcon from '../../../../../../images/resume/contact-icons/email-icon.png';
+import houseIcon from '../../../../../../images/resume/contact-icons/house-icon.png';
+import linkedinIcon from '../../../../../../images/resume/contact-icons/linkedin-icon.png';
+import ContactSection from './ContactSection';
 
 class Contact extends Component {
   render() {
+    const { data } = this.props;
+    const city = data[0] === '' ? '' : data[0][0].city;
+    const state = data[0] === '' ? '' : `, ${data[0][0].state}`;
+
     return (
       <div className="resume-section-container">
         <CategoryTitleRight text="Contact" />
+        <div className="contact-sections-container">
+          <ContactSection
+            imgIcon={phoneIcon}
+            textContent={data[0] === '' ? '' : data[0][0]['phone']}
+            altText={'Phone-icon'}
+          />
+          <ContactSection
+            imgIcon={emailIcon}
+            textContent={data[0] === '' ? '' : data[0][0].email}
+            altText={'Email-icon'}
+          />
+          <ContactSection
+            imgIcon={houseIcon}
+            textContent={city + state}
+            altText={'House-icon'}
+          />
+          <ContactSection
+            imgIcon={linkedinIcon}
+            textContent={data[0] === '' ? '' : data[0][0]['linkedin-link']}
+            altText={'Linkedin-icon'}
+          />
+        </div>
       </div>
     );
   }

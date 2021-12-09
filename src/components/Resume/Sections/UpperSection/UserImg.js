95e4a9c44a130603ea1e9be72bcf4d7993e1960f
Edgar <ecarrillo046@gmail.com>
2021-12-09 09:53:28 -0800

    feat: add image upload

--- src/components/Resume/Sections/UpperSection/UserImg.js
+++ src/components/Resume/Sections/UpperSection/UserImg.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import defaultUserImg from '../../../../images/resume/default-profile-img.jpeg';
 
 class UserImg extends Component {
+  loadImg = (data) => {
+    if (data[0][0].image !== '') {
+      const file = new Blob([data[0][0].image], { type: '.png, .jpeg, .jpg' });
+      return URL.createObjectURL(file);
+    } else {
+      return defaultUserImg;
+    }
+  };
+
   render() {
+    const { data } = this.props;
     return (
       <div className="user-img-container">
-        <img src={defaultUserImg} alt="default user profile img" />
+        <img
+          src={
+            data[0] === '' || undefined ? defaultUserImg : this.loadImg(data)
+          }
+          alt="default user profile img"
+        />
       </div>
     );
   }

da639b1bf808ad187441ed68134684f9e0dccca9
Edgar <ecarrillo046@gmail.com>
2021-12-10 15:30:40 -0800

    feat: add placeholder-text to title-block

--- src/components/Resume/Sections/TitleBlock.js
+++ src/components/Resume/Sections/TitleBlock.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 
 class TitleBlock extends Component {
+  isNotValidData = (propName) => {
+    if (this.props.data[0] === '' || this.props.data[0][0][propName] === '') {
+      return true;
+    }
+    return false;
+  };
+
   render() {
     const { data } = this.props;
     return (
       <div className="title-block">
         <div>
-          <p>{data[0] === '' ? '' : data[0][0].title}</p>
+          <p>{this.isNotValidData('title') ? 'Job Title' : data[0][0].title}</p>
         </div>
         <div>
           <p>
-            {data[0] === '' ? '' : data[0][0]['first-name']}{' '}
-            {data[0] === '' ? '' : data[0][0]['last-name']}
+            {this.isNotValidData('first-name')
+              ? 'John'
+              : data[0][0]['first-name']}{' '}
+            {this.isNotValidData('last-name') ? 'Doe' : data[0][0]['last-name']}
           </p>
         </div>
       </div>

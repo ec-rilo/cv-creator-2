927b3ed6f5d2187bc41544914c14c0bd460dded5
Edgar <ecarrillo046@gmail.com>
2021-12-10 16:00:01 -0800

    feat: Add placeholder values to about description

--- src/components/Resume/Sections/LowerSection/WorkContainer/Description.js
+++ src/components/Resume/Sections/LowerSection/WorkContainer/Description.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 
 class Description extends Component {
+  isNotValidData = (propName) => {
+    if (
+      this.props.data === undefined ||
+      this.props.data[propName] === '' ||
+      this.props.data === ''
+    ) {
+      return true;
+    }
+    return false;
+  };
+
   render() {
+    const { data } = this.props;
+    const placeholderText =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+
     return (
       <div>
         <textarea
           className="experience-text-container"
-          value="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat."
+          value={
+            this.isNotValidData('description')
+              ? placeholderText
+              : data['description']
+          }
           readOnly
         ></textarea>
       </div>

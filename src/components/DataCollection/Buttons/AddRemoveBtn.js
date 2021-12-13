5ee1253b62fca45178b89244e854bcdbb893a921
Edgar <ecarrillo046@gmail.com>
2021-12-13 13:44:29 -0800

    feat: change components into functional components

--- src/components/DataCollection/Buttons/AddRemoveBtn.js
+++ src/components/DataCollection/Buttons/AddRemoveBtn.js
@@ -1,65 +1,59 @@
 import React, { Component } from 'react';
 
-class AddRemoveBtn extends Component {
-  render() {
-    const { createSection, removeSection, sectionKey } = this.props;
-    return (
-      <div className="add-delete-btn-container">
-        <button
-          onClick={() => removeSection(sectionKey)}
-          className="add-delete-btn"
-          type="button"
-        >
-          Delete
-        </button>
-        <button
-          onClick={() => {
-            createSection();
-          }}
-          className="add-delete-btn"
-          type="button"
-        >
-          Add
-        </button>
-      </div>
-    );
-  }
-}
+const AddRemoveBtn = (props) => {
+  const { createSection, removeSection, sectionKey } = props;
+  return (
+    <div className="add-delete-btn-container">
+      <button
+        onClick={() => removeSection(sectionKey)}
+        className="add-delete-btn"
+        type="button"
+      >
+        Delete
+      </button>
+      <button
+        onClick={() => {
+          createSection();
+        }}
+        className="add-delete-btn"
+        type="button"
+      >
+        Add
+      </button>
+    </div>
+  );
+};
 
-class DeleteBtn extends Component {
-  render() {
-    const { removeSection, sectionKey } = this.props;
-    return (
-      <div className="add-delete-btn-container">
-        <button
-          onClick={() => removeSection(sectionKey)}
-          className="add-delete-btn"
-          type="button"
-        >
-          Delete
-        </button>
-      </div>
-    );
-  }
-}
+const DeleteBtn = (props) => {
+  const { removeSection, sectionKey } = props;
+  return (
+    <div className="add-delete-btn-container">
+      <button
+        onClick={() => removeSection(sectionKey)}
+        className="add-delete-btn"
+        type="button"
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
 
-class AddBtn extends Component {
-  render() {
-    const { createSection } = this.props;
-    return (
-      <div className="add-delete-btn-container">
-        <button
-          onClick={() => {
-            createSection();
-          }}
-          className="add-delete-btn"
-          type="button"
-        >
-          Add
-        </button>
-      </div>
-    );
-  }
-}
+const AddBtn = (props) => {
+  const { createSection } = props;
+  return (
+    <div className="add-delete-btn-container">
+      <button
+        onClick={() => {
+          createSection();
+        }}
+        className="add-delete-btn"
+        type="button"
+      >
+        Add
+      </button>
+    </div>
+  );
+};
 
 export { AddRemoveBtn, DeleteBtn, AddBtn };

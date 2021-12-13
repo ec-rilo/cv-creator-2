5c0a65c7260e55a7edd60a7d5ef2c69c2f079f34
Edgar <ecarrillo046@gmail.com>
2021-12-13 13:39:28 -0800

    docs: refactor to use functional components

--- src/components/DataCollection/Inputs/MainInputs.js
+++ src/components/DataCollection/Inputs/MainInputs.js
@@ -1,97 +1,89 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class TextInput extends Component {
-  render() {
-    const { inputInfo, updateData } = this.props;
-    return (
-      <div className="default-input-container">
-        <label htmlFor={inputInfo.forProp}></label>
-        <input
-          type="text"
-          id={inputInfo.forProp}
-          placeholder={inputInfo.text}
-          onChange={(e) => updateData(inputInfo.forProp, e.target.value)}
-          required
-        ></input>
-      </div>
-    );
-  }
-}
+const TextInput = (props) => {
+  const { inputInfo, updateData } = props;
 
-class ImgInput extends Component {
-  render() {
-    const { inputInfo, updateData } = this.props;
+  return (
+    <div className="default-input-container">
+      <label htmlFor={inputInfo.forProp}></label>
+      <input
+        type="text"
+        id={inputInfo.forProp}
+        placeholder={inputInfo.text}
+        onChange={(e) => updateData(inputInfo.forProp, e.target.value)}
+        required
+      ></input>
+    </div>
+  );
+};
 
-    return (
-      <div className="default-input-container img-input">
-        <label htmlFor={inputInfo.forProp}></label>
-        <input
-          type="file"
-          accept=".png, .jpg, .jpeg"
-          id={inputInfo.forProp}
-          placeholder={inputInfo.text}
-          onChange={(e) => updateData(inputInfo.forProp, e.target.files[0])}
-          required
-        ></input>
-        <p>Image</p>
-      </div>
-    );
-  }
-}
+const ImgInput = (props) => {
+  const { inputInfo, updateData } = props;
 
-class TelInput extends Component {
-  render() {
-    const { inputInfo, updateData } = this.props;
+  return (
+    <div className="default-input-container img-input">
+      <label htmlFor={inputInfo.forProp}></label>
+      <input
+        type="file"
+        accept=".png, .jpg, .jpeg"
+        id={inputInfo.forProp}
+        placeholder={inputInfo.text}
+        onChange={(e) => updateData(inputInfo.forProp, e.target.files[0])}
+        required
+      ></input>
+      <p>Image</p>
+    </div>
+  );
+};
 
-    return (
-      <div className="default-input-container">
-        <label htmlFor={inputInfo.forProp}></label>
-        <input
-          type="tel"
-          id={inputInfo.forProp}
-          placeholder={inputInfo.text}
-          onChange={(e) => updateData(inputInfo.forProp, e.target.value)}
-          required
-        ></input>
-      </div>
-    );
-  }
-}
+const TelInput = (props) => {
+  const { inputInfo, updateData } = props;
 
-class EmailInput extends Component {
-  render() {
-    const { inputInfo, updateData } = this.props;
-    return (
-      <div className="default-input-container">
-        <label htmlFor={inputInfo.forProp}></label>
-        <input
-          type="email"
-          id={inputInfo.forProp}
-          placeholder={inputInfo.text}
-          onChange={(e) => updateData(inputInfo.forProp, e.target.value)}
-          required
-        ></input>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="default-input-container">
+      <label htmlFor={inputInfo.forProp}></label>
+      <input
+        type="tel"
+        id={inputInfo.forProp}
+        placeholder={inputInfo.text}
+        onChange={(e) => updateData(inputInfo.forProp, e.target.value)}
+        required
+      ></input>
+    </div>
+  );
+};
 
-class DescriptionInput extends Component {
-  render() {
-    const { inputInfo, updateData } = this.props;
+const EmailInput = (props) => {
+  const { inputInfo, updateData } = props;
 
-    return (
-      <div className="description-input-container">
-        <label htmlFor={inputInfo.forProp}></label>
-        <textarea
-          id={inputInfo.forProp}
-          placeholder={inputInfo.text}
-          onChange={(e) => updateData(inputInfo.forProp, e.target.value)}
-          required
-        ></textarea>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="default-input-container">
+      <label htmlFor={inputInfo.forProp}></label>
+      <input
+        type="email"
+        id={inputInfo.forProp}
+        placeholder={inputInfo.text}
+        onChange={(e) => updateData(inputInfo.forProp, e.target.value)}
+        required
+      ></input>
+    </div>
+  );
+};
+
+const DescriptionInput = (props) => {
+  const { inputInfo, updateData } = props;
+
+  return (
+    <div className="description-input-container">
+      <label htmlFor={inputInfo.forProp}></label>
+      <textarea
+        id={inputInfo.forProp}
+        placeholder={inputInfo.text}
+        onChange={(e) => updateData(inputInfo.forProp, e.target.value)}
+        required
+      ></textarea>
+    </div>
+  );
+};
 
 export { TextInput, ImgInput, TelInput, EmailInput, DescriptionInput };

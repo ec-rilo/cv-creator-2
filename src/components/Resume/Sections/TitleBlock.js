d5afba957146aa0bb809d8efc3d39afd49b6467f
Edgar <ecarrillo046@gmail.com>
2021-12-13 14:04:39 -0800

    docs: refactor using functional component

--- src/components/Resume/Sections/TitleBlock.js
+++ src/components/Resume/Sections/TitleBlock.js
@@ -1,31 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class TitleBlock extends Component {
-  isNotValidData = (propName) => {
-    if (this.props.data[0] === '' || this.props.data[0][0][propName] === '') {
+const TitleBlock = (props) => {
+  const { data } = props;
+  const isNotValidData = (propName) => {
+    if (data[0] === '' || data[0][0][propName] === '') {
       return true;
     }
     return false;
   };
 
-  render() {
-    const { data } = this.props;
-    return (
-      <div className="title-block">
-        <div>
-          <p>{this.isNotValidData('title') ? 'Job Title' : data[0][0].title}</p>
-        </div>
-        <div>
-          <p>
-            {this.isNotValidData('first-name')
-              ? 'John'
-              : data[0][0]['first-name']}{' '}
-            {this.isNotValidData('last-name') ? 'Doe' : data[0][0]['last-name']}
-          </p>
-        </div>
+  return (
+    <div className="title-block">
+      <div>
+        <p>{isNotValidData('title') ? 'Job Title' : data[0][0].title}</p>
       </div>
-    );
-  }
-}
+      <div>
+        <p>
+          {isNotValidData('first-name') ? 'John' : data[0][0]['first-name']}{' '}
+          {isNotValidData('last-name') ? 'Doe' : data[0][0]['last-name']}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default TitleBlock;

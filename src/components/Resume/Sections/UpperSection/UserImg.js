f9689e35799831e376a2de329102eddc94004f41
Edgar <ecarrillo046@gmail.com>
2021-12-13 14:14:02 -0800

    refactor to use functional component

--- src/components/Resume/Sections/UpperSection/UserImg.js
+++ src/components/Resume/Sections/UpperSection/UserImg.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import defaultUserImg from '../../../../images/resume/default-profile-img.jpeg';
 
-class UserImg extends Component {
-  loadImg = (data) => {
+const UserImg = (props) => {
+  const { data } = props;
+  const loadImg = (data) => {
     if (data[0][0]['image-file'] !== '') {
       const file = new Blob([data[0][0]['image-file']], {
         type: '.png, .jpeg, .jpg',
@@ -13,19 +14,14 @@ class UserImg extends Component {
     }
   };
 
-  render() {
-    const { data } = this.props;
-    return (
-      <div className="user-img-container">
-        <img
-          src={
-            data[0] === '' || undefined ? defaultUserImg : this.loadImg(data)
-          }
-          alt="default user profile img"
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="user-img-container">
+      <img
+        src={data[0] === '' || undefined ? defaultUserImg : loadImg(data)}
+        alt="default user profile img"
+      />
+    </div>
+  );
+};
 
 export default UserImg;

a4124a7e467e3c4e55a1fb0460eb437ebddb08b4
Edgar <ecarrillo046@gmail.com>
2021-12-12 21:30:50 -0800

    docs: refactor component to be a functional component

--- src/components/DataCollection/Sections/Skill.js
+++ src/components/DataCollection/Sections/Skill.js
@@ -1,84 +1,61 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { SkillSection } from './Sections';
 import { AddBtn } from '../Buttons/AddRemoveBtn';
 import uniqid from 'uniqid';
 
-class Skill extends Component {
-  constructor(props) {
-    super(props);
+const Skill = (props) => {
+  const { updateCategories } = props;
+  const key1 = uniqid();
+  const key2 = uniqid();
+  const [skillSections, setSkillSections] = useState([
+    {
+      key: key1,
+      sectionKey: key1,
+      isMainSection: true,
+    },
+  ]);
+  const [section, setSection] = useState({
+    key: key2,
+    sectionKey: key2,
+    isMainSection: true,
+  });
 
-    const key1 = uniqid();
-    const key2 = uniqid();
-    this.state = {
-      skillSections: [
-        {
-          key: key1,
-          sectionKey: key1,
-          isMainSection: true,
-        },
-      ],
-      section: {
-        key: key2,
-        sectionKey: key2,
-        isMainSection: true,
-      },
-    };
-
-    this.createSection = this.createSection.bind(this);
-    this.removeSection = this.removeSection.bind(this);
-    this.updateSkillsSection = this.updateSkillsSection.bind(this);
-  }
-
-  createSection = () => {
-    this.setState({
-      skillSections: this.state.skillSections
-        .concat(this.state.section)
-        .map((section, index, array) => {
-          if (index === array.length - 1) {
-            section.isMainSection = true;
-            return section;
-          } else {
-            section.isMainSection = false;
-            return section;
-          }
-        }),
-    });
-
-    const newKey = uniqid();
-    this.setState({
-      section: {
-        key: newKey,
-        sectionKey: newKey,
-        isMainSection: true,
-      },
-    });
+  const createSection = () => {
+    setSkillSections(
+      skillSections.concat(section).map((section, index, array) => {
+        if (index === array.length - 1) {
+          section.isMainSection = true;
+          return section;
+        } else {
+          section.isMainSection = false;
+          return section;
+        }
+      })
+    );
   };
 
-  updateSkillsSection = (section, key) => {
-    let tempArr = [...this.state.skillSections];
+  const updateSkillsSection = (section, key) => {
+    let tempArr = [...skillSections];
     tempArr.forEach((obj) => {
       if (obj.key === key) {
         obj.data = section;
       }
     });
 
-    this.setState(
-      {
-        skillSections: tempArr,
-      },
-      () => {
-        let newArr = [];
-        this.state.skillSections.forEach((section) => {
-          newArr.push(section.data);
-        });
-        this.props.updateCategories('Skills', newArr);
-      }
-    );
+    setSkillSections(tempArr);
   };
+  useEffect(() => {
+    let newArr = [];
+    skillSections.forEach((section) => {
+      newArr.push(section.data);
+    });
 
-  removeSection = (sectionId) => {
-    this.setState({
-      skillSections: this.state.skillSections
+    updateCategories('Skills', newArr);
+  }, [skillSections]);
+
+  const removeSection = (sectionId) => {
+    setSkillSections(
+      skillSections
         .filter((section) => section.sectionKey !== sectionId)
         .map((section, index, array) => {
           if (index === array.length - 1) {
@@ -88,37 +65,34 @@ class Skill extends Component {
             section.isMainSection = false;
             return section;
           }
-        }),
-    });
+        })
+    );
   };
 
-  render() {
-    const { skillSections } = this.state;
-    if (skillSections.length === 0) {
-      return (
-        <div>
-          <AddBtn sectionKey={uniqid()} createSection={this.createSection} />
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          {skillSections.map((section) => {
-            return (
-              <SkillSection
-                key={section.key}
-                sectionKey={section.sectionKey}
-                isMainSection={section.isMainSection}
-                createSection={this.createSection}
-                removeSection={this.removeSection}
-                updateSkillsSection={this.updateSkillsSection}
-              />
-            );
-          })}
-        </div>
-      );
-    }
+  if (skillSections.length === 0) {
+    return (
+      <div>
+        <AddBtn sectionKey={uniqid()} createSection={createSection} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {skillSections.map((section) => {
+          return (
+            <SkillSection
+              key={section.key}
+              sectionKey={section.sectionKey}
+              isMainSection={section.isMainSection}
+              createSection={createSection}
+              removeSection={removeSection}
+              updateSkillsSection={updateSkillsSection}
+            />
+          );
+        })}
+      </div>
+    );
   }
-}
+};
 
 export default Skill;

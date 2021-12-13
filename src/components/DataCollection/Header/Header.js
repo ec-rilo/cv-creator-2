48780305fdd72989261cee4e9a926464632c2a88
Edgar <ecarrillo046@gmail.com>
2021-12-13 13:41:14 -0800

    docs: change into functional component

--- src/components/DataCollection/Header/Header.js
+++ src/components/DataCollection/Header/Header.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Header extends Component {
-  render() {
-    return (
-      <header>
-        <h1>CV Creator</h1>
-        <p>Thank you for visiting! Please fill out the information below</p>
-      </header>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <header>
+      <h1>CV Creator</h1>
+      <p>Thank you for visiting! Please fill out the information below</p>
+    </header>
+  );
+};
 
 export default Header;

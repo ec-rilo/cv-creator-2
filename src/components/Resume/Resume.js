81c12d7c9a95d252f2417cf70fa423bda669a2dd
Edgar <ecarrillo046@gmail.com>
2021-12-13 13:53:45 -0800

    docs: turn into a functional component

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../../styles/Resume.css';
 import UpperSection from './Sections/UpperSection/UpperSection';
 import TitleBlock from './Sections/TitleBlock';
 import LowerSection from './Sections/LowerSection/LowerSection';
 
-class Resume extends Component {
-  render() {
-    const { data } = this.props;
-    return (
-      <div className="resume-container">
-        <UpperSection data={data} />
-        <TitleBlock data={data} />
-        <LowerSection data={data} />
-      </div>
-    );
-  }
-}
+const Resume = (props) => {
+  const { data } = props;
+  return (
+    <div className="resume-container">
+      <UpperSection data={data} />
+      <TitleBlock data={data} />
+      <LowerSection data={data} />
+    </div>
+  );
+};
 
 export default Resume;
